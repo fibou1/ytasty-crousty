cbf7e15ea6f29d2a94240e8c089285c7c408623f
fibou1 <firas@Redmove01.localdomain>
2026-07-23 10:12:27 +0200

    feat:Créer seed.py avec les 3 restaurants(pour remplir le db avec des données initiales

---
+++ backend/seed.py
@@ -0,0 +1,51 @@
+with SessionLocal() as session:
+    nombre = session.query(Restaurant).count()
+    if nombre >= 3:
+        print("c ok Les 3 restaurants sont déjà présents.")
+    else:
+        # Créer un utilisateur avec des articles
+        paris = Restaurant(
+            uid_resto="resto1",
+            nom="Le Gourmet",
+            adress="123 Rue de la Cuisine, Paris",
+            status_ouvert=True,
+            horaire_ouverture=time(9, 0),
+            horaire_fermeture=time(22, 0),
+            email="leGourmet@gmail.com",
+            numero=123456789
+        )
+        session.add(paris)
+        aix = Restaurant(
+            uid_resto="resto2",
+            nom="Le Gourmet",
+            adress="123 Rue de la Cuisine, Aix-en-Provence",
+            status_ouvert=True,
+            horaire_ouverture=time(9, 0),
+            horaire_fermeture=time(22, 0),
+            email="leGourmet@gmail.com",
+            numero=123456789
+        )
+        session.add(aix)
+        lyon = Restaurant(
+            uid_resto="resto3",
+            nom="Le Gourmet",
+            adress="123 Rue de la Cuisine, Lyon",
+            status_ouvert=True,
+            horaire_ouverture=time(9, 0),
+            horaire_fermeture=time(22, 0),
+            email="leGourmet@gmail.com",
+            numero=123456789
+        )
+        session.add(lyon)
+        print(f"Créé : {paris}")
+        print(f"Créé : {aix}")
+        print(f"Créé : {lyon}")
+        
+        session.commit()   
+        pass
+        
+
+
+
+
+
